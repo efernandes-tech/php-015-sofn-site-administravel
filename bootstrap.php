83dc9b25bd6cf593a0e0182d6b43c19b90f9a357
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-02-28 23:14:21 -0300

    organizando funcoes

--- bootstrap.php
+++ bootstrap.php
@@ -1,23 +1,8 @@
 <?php
 
-function resolve($route)
-{
-    $path = $_SERVER['PATH_INFO'] ?? '/';
-
-    $route = '/^' . str_replace('/', '\/', $route) . '$/';
-
-    if (preg_match($route, $path, $params)) {
-        return $params;
-    }
-
-    return false;
-}
-
-function render($content, $template, $data = [])
-{
-    $content = __DIR__ . '/templates/' . $content . '.tpl.php';
-    return include __DIR__ . '/templates/' . $template . '.tpl.php';
-}
+require __DIR__ . '/src/resolve-route.php';
+require __DIR__ . '/src/render.php';
+require __DIR__ . '/src/connection.php';
 
 if (resolve('/admin/?(.*)')) {
 
---
+++ src/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+mysqli_report(MYSQLI_REPORT_ERROR);
+
+$conn = new mysqli(
+    'localhost',
+    'root',
+    '',
+    'php-015-sofn-site-administravel'
+);
