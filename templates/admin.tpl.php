9b35402cb7fb716b41caf802f74fa77289b1bb08
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-03-01 00:17:43 -0300

    iniciando template do painel administrativo

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -9,7 +9,30 @@
     <title>Painel Administrativo</title>
 </head>
 <body>
-    <?php include $content; ?>
+    <div id="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="" class="navbar-brand">Admin</a>
+            <span class="navbar-text">Painel Administrativo</span>
+        </nav>
+    </div>
+    <div id="main">
+        <div class="row">
+            <div class="col">
+                <ul class="nav flex-column nav-pills bg-secondary text-white p-2">
+                    <li class="nav-item">
+                        <span class="nav-link text-white-50"><small>Menu</small></span>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">Páginas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Usuários</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-10"><?php include $content; ?></div>
+        </div>
+    </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
