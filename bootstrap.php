979818aca6d9005b584e13a76c51258d881ed00c
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-02-28 21:56:28 -0300

    separando urls amigaveis do site e da administracao

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,8 @@ $path = $_SERVER['PATH_INFO'] ?? '/';
 
 if ($path == '/') {
     require __DIR__ . '/site/routes.php';
+} else if ($path == '/admin') {
+    require __DIR__ . '/admin/routes.php';
 } else {
     echo "Página não encontrada!";
 }
