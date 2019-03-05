7cf8c522576c3e0bb661b0bde24b62c7f720082b
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-03-05 11:33:42 -0300

    template do site

--- templates/site.tpl.php
+++ templates/site.tpl.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <link rel="stylesheet" href="">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/site.css">
 </head>
 <body>
-    <h1>Bem-vindo!</h1>
-    <?php include $content; ?>
+    <header id="header">
+        <h1>Bem vindo</h1>
+    </header>
 
-    <p><small>2019 - todos os direitos reservados.</small></p>
+    <ul id="nav">
+        <li><a href="/">Home</a></li>
+        <li><a href="/contato">Contato</a></li>
+    </ul>
+
+    <main id="content">
+        <?php include $content; ?>
+    </main>
+
+    <p id="footer"><small><?php echo date('Y') ?> - todos os direitos reservados</small></p>
 </body>
 </html>
