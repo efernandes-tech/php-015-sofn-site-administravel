5ec5f09f5a25d252507654769217a769f7a79459
Éderson Fernandes <edersonluis.rf@gmail.com>
2019-03-05 13:43:42 -0300

    deploy

--- config.php
+++ config.php
@@ -1,7 +1,13 @@
 <?php
 
+// define('DEBUG', true);
+// define('DB_SERVER', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASSWD', '');
+// define('DB_NAME', 'php-015-sofn-site-administravel');
+
 define('DEBUG', true);
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWD', '');
-define('DB_NAME', 'php-015-sofn-site-administravel');
+define('DB_SERVER', 'g8mh6ge01lu2z3n1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USER', 'kdv6ucuwcrt6bpam');
+define('DB_PASSWD', 'gx38bvhkt4l50dof');
+define('DB_NAME', 'o85rd1p4r7kex99j');
